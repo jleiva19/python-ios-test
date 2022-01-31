dffe2b15cae94be10535314df295e8340fa9c765
jleiva19 <jorgels.contact@gmail.com>
2022-01-31 19:01:22 -0300

    Changed everything to an actual python file.

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-print("hello Git")
-print("Hello from m-o.")
-print("hello again from Iphone")
-print("final try")
-print("this time it is")
-print("please work")
-print("I gave up and decided to make the repo public, lel.")
-print("Duplicated this line for a quick test.")
-print("editing file from termius")
-print("gonna test the personal token")
-print("boi")
-print("hello 2")
-print ("Hello, I type from my keyboard")
+def greeting():
+    user_name = input("What's your name?\n")
+    print(f"Hello {user_name}")
+
+greeting()
