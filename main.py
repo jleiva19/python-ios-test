f2549755a9ec5ef5214b8dcee76b3b78f383c323
jleiva19 <jorgels.contact@gmail.com>
2022-01-31 01:47:53 -0300

    I did this from my phone

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ print("this time it is")
 print("please work")
 print("I gave up and decided to make the repo public, lel.")
 print("Duplicated this line for a quick test.")
+print("editing file from termius")
+
